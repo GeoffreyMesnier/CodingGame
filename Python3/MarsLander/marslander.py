f90f26173b919f260d340a2ee966a46a3405552f
Geoffrey MESNIER <44602983+GeoffreyMesnier@users.noreply.github.com>
2020-05-29 16:02:53 +0200

    Update marslander.py

--- Python3/MarsLander/marslander.py
+++ Python3/MarsLander/marslander.py
@@ -1,18 +1,17 @@
 import sys
 import math
 
-# Auto-generated code below aims at helping you parse
-# the standard input according to the problem statement.
-
 surface_n = int(input())  # the number of points used to draw the surface of Mars.
 for i in range(surface_n):
     # land_x: X coordinate of a surface point. (0 to 6999)
     # land_y: Y coordinate of a surface point. By linking all the points together in a sequential fashion, you form the surface of Mars.
     land_x, land_y = [int(j) for j in input().split()]
 
+# Initialise les variables
 pFusee = 0
 angle = 0
 result=""
+
 # game loop
 while True:
     # h_speed: the horizontal speed (in m/s), can be negative.
@@ -21,17 +20,18 @@ while True:
     # rotate: the rotation angle in degrees (-90 to 90).
     # power: the thrust power (0 to 4).
     x, y, h_speed, v_speed, fuel, rotate, power = [int(i) for i in input().split()]
-
+    
+    # Augment la puissance de la fusée
     if  abs(v_speed) > 40:
         if pFusee < 4:
             pFusee = pFusee +1
+            
+    # Diminue la puissance de la fusée
     elif abs(v_speed) < 40:
         if pFusee < 0:
             pFusee = pFusee -1
 
-
-    # Write an action using print
-    # To debug: print("Debug messages...", file=sys.stderr)
-
     result = str(angle) + " " + str(pFusee)   # 2 integers: rotate power. rotate is the desired rotation angle (should be 0 for level 1), power is the desired thrust power (0 to 4).
+    
+    # Affiche le résultat
     print(result)
