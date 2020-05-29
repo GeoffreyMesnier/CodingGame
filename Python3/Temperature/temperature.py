7b54f7e366cb59feb2723c740c0354d78c79e622
Geoffrey MESNIER <44602983+GeoffreyMesnier@users.noreply.github.com>
2020-05-29 16:04:47 +0200

    Update temperature.py

--- Python3/Temperature/temperature.py
+++ Python3/Temperature/temperature.py
@@ -1,16 +1,15 @@
 import sys
 import math
 
-# Auto-generated code below aims at helping you parse
-# the standard input according to the problem statement.
 tempsMin=0
 n = int(input())  # the number of temperatures to analyse
+# Parcours toutes les températures
 for i in input().split():
+    
     # t: a temperature expressed as an integer ranging from -273 to 5
-
     t = int(i)
 
-    #Gere le cas aucune temperature
+    #Gere exception si aucune temperature
     if n == 0:
         tempsMin=0
     elif tempsMin == 0:
@@ -20,8 +19,6 @@ for i in input().split():
     elif abs(tempsMin) == abs(t):
         if t > tempsMin:
             tempsMin = t
-
-# Write an action using print
-# To debug: print("Debug messages...", file=sys.stderr)
-
+            
+# Affiche le résultat
 print(tempsMin)
